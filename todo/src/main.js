27d069f44df248dc32c65d39cc251a102a66b275
lossp <richard.xxmxx@gmail.com>
2017-09-23 14:57:11 +0800

    模版添加，基本功能实现，增删改除，以及全选，左下角右下角按钮功能实现

---
+++ todo/src/main.js
@@ -0,0 +1,90 @@
+import 'todomvc-app-css/index.css'
+import Vue from 'vue'
+
+
+var filters = {
+  all(todos){
+    return todos
+  },
+  active(todos){
+    return todos.filter((todo)=>{
+      return  !todo.completed
+    })
+  },
+  completed(todos){
+    return todos.filter((todo)=>{
+      return todo.completed
+    })
+  }
+}
+
+new Vue({
+  el:'.todoapp',
+  data(){
+    return {
+      message: 'hello',
+      title: 'New Todo',
+      newTodo: 'Vue',
+      todos:[{
+        content:'vue',
+        completed:false
+      }],
+      editedTodo:null,
+      hashName :'all'
+    }
+  },
+  methods:{
+    addTodo(){
+      if(!this.newTodo){
+        return
+      }
+      this.todos.push({content:this.newTodo,completed: false});
+      this.newTodo = '';
+    },
+    removeTodo(index){
+      this.todos.splice(index,1)
+    },
+    editTodo(todo){
+      this.editCache = todo.content
+      this.editedTodo = todo
+    },
+    doneEdit(todo,index){
+      this.editedTodo = null
+      if(!todo.content){
+        this.removeTodo(index)
+      }
+    },
+    cancelEdit(todo){
+      this.editedTodo = null
+      todo.content = this.editCache
+    },
+    clear(){
+      this.todos = filters.active(this.todos)
+    }
+  },
+  directives:{
+    focus(el,value){
+      if(value){
+        el.focus()
+      }
+    }
+  },
+  computed:{
+    remain(){
+      return filters.active(this.todos).length;
+    },
+    isAll:{
+      get(){
+        return this.remain === 0
+      },
+      set(value){
+        this.todos.forEach((todo)=>{
+          todo.completed = value
+        })
+      }
+    },
+    filteredTodos(){
+
+    }
+  }
+})
